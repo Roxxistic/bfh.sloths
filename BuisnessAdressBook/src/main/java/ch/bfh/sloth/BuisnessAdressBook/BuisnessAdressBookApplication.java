ede2cc7912ab1abed28b47f937857889b461c4cc
ammop2 <pascal.ammon@gmail.com>
2017-11-16 16:22:57 +0100

    add inital version

---
+++ BuisnessAdressBook/src/main/java/ch/bfh/sloth/BuisnessAdressBook/BuisnessAdressBookApplication.java
@@ -0,0 +1,12 @@
+package ch.bfh.sloth.BuisnessAdressBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuisnessAdressBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuisnessAdressBookApplication.class, args);
+	}
+}
