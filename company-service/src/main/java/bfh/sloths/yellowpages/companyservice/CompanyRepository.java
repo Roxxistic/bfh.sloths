d484b796b2200a82bfb861e34848a10014b41dc1
Marc Rey <roxxistic@gmail.com>
2018-01-20 17:35:47 +0100

    removes superfluous services.

--- company-service/src/main/java/bfh/sloths/yellowpages/companyservice/CompanyRepository.java
+++
@@ -1,7 +0,0 @@
-package bfh.sloths.yellowpages.companyservice;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface CompanyRepository extends CrudRepository<Company, String> {
-
-}
--- person-service/src/main/java/bfh/sloths/yellowpages/personservice/PersonRepository.java
+++
@@ -1,7 +0,0 @@
-package bfh.sloths.yellowpages.personservice;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface PersonRepository extends CrudRepository<Person, String> {
-
-}
