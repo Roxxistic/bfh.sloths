6d97fb9db9b1bd6db568b7e4aea4039e266241d3
Marc Rey <roxxistic@gmail.com>
2018-01-20 16:24:30 +0100

    adds minimal setup for company-service (model, repo, id exposure).

---
+++ company-service/src/main/java/bfh/sloths/yellowpages/companyservice/CompanyRepository.java
@@ -0,0 +1,7 @@
+package bfh.sloths.yellowpages.companyservice;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CompanyRepository extends CrudRepository<Company, String> {
+
+}
--- company-service/src/main/java/bfh/sloths/yellowpages/companyservice/CompanyServiceApplication.java
+++ company-service/src/main/java/bfh/sloths/yellowpages/companyservice/CompanyServiceApplication.java
@@ -2,11 +2,19 @@ package bfh.sloths.yellowpages.companyservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
 
 @SpringBootApplication
-public class CompanyServiceApplication {
+@EnableEurekaClient
+public class CompanyServiceApplication extends RepositoryRestConfigurerAdapter {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CompanyServiceApplication.class, args);
+	    SpringApplication.run(CompanyServiceApplication.class, args);
 	}
+
+	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config){
+	    config.exposeIdsFor(Company.class);
+    }
 }
