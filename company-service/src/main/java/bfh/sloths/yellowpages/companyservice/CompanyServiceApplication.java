4a27d588e4cb8ec197fcc8335ec57b5ca05501ee
Marc Rey <roxxistic@gmail.com>
2018-01-20 16:09:59 +0100

    initializes project for company-service.

---
+++ company-service/src/main/java/bfh/sloths/yellowpages/companyservice/CompanyServiceApplication.java
@@ -0,0 +1,12 @@
+package bfh.sloths.yellowpages.companyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyServiceApplication.class, args);
+	}
+}
