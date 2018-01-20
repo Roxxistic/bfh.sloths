4dcd4801ab5c90a4da24a477bb5f1ac6ac68c08e
Marc Rey <roxxistic@gmail.com>
2018-01-20 23:12:12 +0100

    setup service that combines partners and contact data.

---
+++ partner-contactdata-service/src/main/java/bfh/sloths/partnercontactdataservice/PartnerContactdataServiceApplication.java
@@ -0,0 +1,12 @@
+package bfh.sloths.partnercontactdataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartnerContactdataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartnerContactdataServiceApplication.class, args);
+	}
+}
