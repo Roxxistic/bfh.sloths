bfd8b675daa1d5346ac70029ee10542a52c8bb23
Marc Rey <roxxistic@gmail.com>
2018-01-27 10:28:53 +0100

    adds some basic classes and annotations.

--- frontend-service/src/main/java/ch/bfh/sloths/yellowpages/frontendservice/FrontendServiceApplication.java
+++ frontend-service/src/main/java/ch/bfh/sloths/yellowpages/frontendservice/FrontendServiceApplication.java
@@ -2,8 +2,12 @@ package ch.bfh.sloths.yellowpages.frontendservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
 public class FrontendServiceApplication {
 
 	public static void main(String[] args) {
