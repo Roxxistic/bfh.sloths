30c3e8b5b85bbbb83f0dde0d1d201c87be2f50b6
Marc Rey <roxxistic@gmail.com>
2018-01-27 10:28:46 +0100

    adds some basic classes and annotations.

--- registry-service/src/main/java/ch/bfh/sloths/yellowpages/registryservice/RegistryServiceApplication.java
+++ registry-service/src/main/java/ch/bfh/sloths/yellowpages/registryservice/RegistryServiceApplication.java
@@ -2,8 +2,10 @@ package ch.bfh.sloths.yellowpages.registryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class RegistryServiceApplication {
 
 	public static void main(String[] args) {
