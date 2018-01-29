2a149a2fc0a9016dcf08c162f4f4b1bb393af32c
Marc Rey <roxxistic@gmail.com>
2018-01-29 08:27:20 +0100

    enables eureka client.

--- partner-service/src/main/java/ch/bfh/sloths/yellowpages/partnerservice/PartnerServiceApplication.java
+++ partner-service/src/main/java/ch/bfh/sloths/yellowpages/partnerservice/PartnerServiceApplication.java
@@ -1,14 +1,24 @@
 package ch.bfh.sloths.yellowpages.partnerservice;
 
+import ch.bfh.sloths.yellowpages.partnerservice.models.Company;
+import ch.bfh.sloths.yellowpages.partnerservice.models.Person;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-public class PartnerServiceApplication {
+@EnableEurekaClient
+public class PartnerServiceApplication extends RepositoryRestConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PartnerServiceApplication.class, args);
 	}
+
+	@Override
+	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config){
+	    config.exposeIdsFor(Person.class, Company.class);
+    }
 }
