f359a798d4e6fb7eed5737635156bd3fea3220ce
Marc Rey <roxxistic@gmail.com>
2018-01-30 19:30:36 +0100

    adapts ym according to demo.

--- partner-service/src/main/resources/data.sql
+++ partner-service/src/main/resources/data.sql
@@ -1,5 +1,11 @@
+-- noinspection SqlNoDataSourceInspectionForFile
+
 /* no manual inclusion needed. spring boot handles that. */
 INSERT INTO PERSON(ID, FIRSTNAME, LASTNAME) VALUES ('person1', 'Homer', 'Simpson');
 INSERT INTO PERSON(ID, FIRSTNAME, LASTNAME) VALUES ('person2', 'Marge', 'Simpson');
+INSERT INTO PERSON(ID, FIRSTNAME, LASTNAME) VALUES ('person3', 'Bart', 'Simpson');
+INSERT INTO PERSON(ID, FIRSTNAME, LASTNAME) VALUES ('person4', 'Maggie', 'Simpson');
+INSERT INTO PERSON(ID, FIRSTNAME, LASTNAME) VALUES ('person5', 'Lisa', 'Simpson');
 INSERT INTO COMPANY(ID, NAME) VALUES ('company1', 'Kwik-E-Mart');
-INSERT INTO COMPANY(ID, NAME) VALUES ('company2', 'Book Store');
+INSERT INTO COMPANY(ID, NAME) VALUES ('company2', 'Comic Book Store');
+INSERT INTO COMPANY(ID, NAME) VALUES ('company3', 'Power Plant');
